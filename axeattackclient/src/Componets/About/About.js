20a7a37a1ace723218f25044b33a027488b5aaaf
BENCO\mb6410 <mburridge@benco.com>
2019-02-14 15:22:03 -0500

    removing test changes

--- axeattackclient/src/Componets/About/About.js
+++ axeattackclient/src/Componets/About/About.js
@@ -11,7 +11,7 @@ function About(props){
     const {classes} = props; 
     return(
         <div>
-            This is the start of the about page. testing
+            This is the start of the about page.
         </div>
     );
 }
